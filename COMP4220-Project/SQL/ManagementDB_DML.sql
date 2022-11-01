ad6bf12b9a7a078745198345dfb65ac2cc4a16e7
Noor-Alina <AN10_23@hotmail.com>
2022-11-01 11:47:36 -0400

    add insert statemments

--- COMP4220-Project/SQL/ManagementDB_DML.sql
+++ COMP4220-Project/SQL/ManagementDB_DML.sql
@@ -1,2 +1,11 @@
 --Insert Into studentInfo
-INSERT INTO 
+INSERT INTO studentInfo(167934082, "John", "Smith", "abc12@uwindsor.ca"); 
+INSERT INTO studentInfo(172834932, "Jane", "Doe", "jane12@uwindsor.ca"); 
+INSERT INTO studentInfo(163048921, "Andy", "Carson", "Carsa89@uwindsor.ca"); 
+INSERT INTO studentInfo(100000231, "Natalie", "Peters", "Peters@uwindsor.ca"); 
+INSERT INTO studentInfo(189023982, "Kim", "Zhang", "Zhangk9@uwindsor.ca"); 
+INSERT INTO studentInfo(167584085, "Julie", "Adams", "AdamsJ@uwindsor.ca"); 
+INSERT INTO studentInfo(167298382, "Nate", "Wyatt", "Wyattn23@uwindsor.ca"); 
+INSERT INTO studentInfo(100823088, "Mike", "Smith", "Msm89@uwindsor.ca"); 
+INSERT INTO studentInfo(182190687, "Ben", "Tate", "TateB4@uwindsor.ca"); 
+INSERT INTO studentInfo(289171390, "Sarah", "Jones", "Jonessa@uwindsor.ca"); 
