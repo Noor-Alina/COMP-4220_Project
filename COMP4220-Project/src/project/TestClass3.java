ee796e25a0ed3cd5fb927bc224d17f5cdf68bc4a
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-11-01 08:38:13 -0400

    Added 4 test classes

---
+++ COMP4220-Project/src/project/TestClass3.java
@@ -0,0 +1,14 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClass3 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ COMP4220-Project/src/project/TestClass4.java
@@ -0,0 +1,14 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClass4 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
