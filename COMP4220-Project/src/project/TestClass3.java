4ff9014a926a4c7664b8b9a5febd30cbcc1e5a94
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-11-01 09:38:38 -0400

    TestClass3 test case added

--- COMP4220-Project/src/project/TestClass3.java
+++ COMP4220-Project/src/project/TestClass3.java
@@ -6,9 +6,51 @@ import org.junit.jupiter.api.Test;
 
 class TestClass3 {
 
+	LibraryManagement lm = new LibraryManagement();
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCase1() throws InputException, DatabaseException{
+		
+		assertEquals(“Reservation made, Reservation# 56690”, lm.reserveOutOfStock(167934082, 4672895719, 15561, "abc12@uwindsor.ca"));
 	}
+	
+	@Test
+	void testCase2(){
+		
+		InputException thrown = assertThrows(
+				InputException.class,
+		           () -> lm.reserveOutOfStock(5, 2, "5A7", "abc12@gmail.com"));
 
+		    assertTrue(thrown.getMessage().contains("Invalid Input"));
+	}
+	
+	@Test
+	void testCase3(){
+		
+		InputException thrown = assertThrows(
+				InputException.class,
+		           () -> lm.reserveOutOfStock(5, 467985719, 15781, "abc12@uwindsor.ca"));
+
+		    assertTrue(thrown.getMessage().contains("Invalid Input"));
+	}
+	
+	@Test
+	void testCase4(){
+		
+		DatabaseException thrown = assertThrows(
+				DatabaseException.class,
+		           () -> lm.reserveOutOfStock(167937082, 4672235719, 12181, "abc12@uwindsor.ca"));
+
+		    assertTrue(thrown.getMessage().contains("Student Number Not Found"));
+	}
+	
+	@Test
+	void testCase5(){
+		
+		InputException thrown = assertThrows(
+				InputException.class,
+		           () -> lm.reserveOutOfStock(167937154, 4672235345, 16475, "abcd@gmail.com"));
+
+		    assertTrue(thrown.getMessage().contains("Invalid Input"));
+	}
 }
