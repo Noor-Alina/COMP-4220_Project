24601c8e48f2efdcb18213eebe6c265c529d8d94
Noor-Alina <AN10_23@hotmail.com>
2022-11-01 11:21:08 -0400

    DB update

--- COMP4220-Project/SQL/ManagementDB.sql
+++ COMP4220-Project/SQL/ManagementDB.sql
@@ -1,6 +1,7 @@
 --create student info table 
 CREATE TABLE studentInfo{
-    student_id INTEGER PRIMARY KEY,
+    student_id NUMBER(9) PRIMARY KEY,
     first_name VARCHAR2(30),
-    last_name VARCHAR2
+    last_name VARCHAR2(30),
+    e-mail VARCHAR2(30)
 }
