5561c06a2cad840c4b819e3945e85664c82bf984
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-11-06 19:37:19 -0500

    Edited the database

--- COMP4220-Project/SQL/ManagementDB_CreateTables.sql
+++ COMP4220-Project/SQL/ManagementDB_CreateTables.sql
@@ -1,7 +1,10 @@
---create student info table 
-CREATE TABLE studentInfo{
-    student_id NUMBER(9) PRIMARY KEY,
-    first_name VARCHAR2(30),
-    last_name VARCHAR2(30),
-    e-mail VARCHAR2(30)
-};
+CREATE DATABASE bookmanagement;
+
+use bookmanagement;
+
+CREATE TABLE StudentInfo (
+    student_id INTEGER(9) PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(30)
+);
