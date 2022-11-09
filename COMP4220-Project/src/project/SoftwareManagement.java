2130a37fa4c2fcc7c0d2d861354cfb836573957a
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-11-08 19:36:59 -0500

    Added dbms integration

--- COMP4220-Project/src/project/SoftwareManagement.java
+++ COMP4220-Project/src/project/SoftwareManagement.java
@@ -1,6 +1,31 @@
 package project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class SoftwareManagement extends Throwable{
 
-	
+	public static void main(String args[]) throws SQLException{
+		
+		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/bookmanagement", "guest", "guest123");
+        Statement st = connect.createStatement();
+        String sql = "SELECT * from studentinfo";
+
+        ResultSet rs = st.executeQuery(sql);
+        
+        while(rs.next()) {
+        	
+        	String dataString= "";
+        	
+        	for(int i= 1; i <= 4; i++) {
+        		
+        		dataString += rs.getString(i) + ":";
+        	}
+        	
+        	System.out.println(dataString);
+        }
+	}
 }
