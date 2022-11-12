dd30804040e9d74a02d7453d2c683aef3904992d
Alina <An10_23@hotmail.com>
2022-11-12 17:51:02 -0500

    Add data for bookInfo table

--- COMP4220-Project/SQL/ManagementDB_DML.sql
+++ COMP4220-Project/SQL/ManagementDB_DML.sql
@@ -22,6 +22,18 @@ VALUES (182190687, 'Ben', 'Tate', 'TateB4@uwindsor.ca');
 INSERT INTO studentInfo
 VALUES (289171390, 'Sarah', 'Jones', 'Jonessa@uwindsor.ca');
 
+# Data Entry for BookInfo
+INSERT INTO BookInfo VALUES (1305266641, 'Multivariable Calculus', 'James', 'Stewart','Cengage Learning', 161.95, 20, 0);
+INSERT INTO BookInfo VALUES (1780176857, 'Intro to Probability & Statistics', 'William', 'Mendenhall', 'Cengage Learning', 141.22, 10, 0);
+INSERT INTO BookInfo VALUES (1780262046, 'Introduction to Algorithms', 'Thomas', 'Cormen', 'MIT Press', 161.95, 2, 2);
+INSERT INTO BookInfo VALUES (1780735211, 'Atomic Habits', 'James', 'Clear', 'Penguin Random House', 16.99, 0, 5);
+INSERT INTO BookInfo VALUES (1780857197, 'The Psychology of Money', 'Morgan', 'Housel', 'Harriman House', 14.00, 0, 5);
+INSERT INTO BookInfo VALUES (1780399592, 'Atlas of the Heart', 'Brene', 'Brown', 'HarperCollins', 24.90, 0, 10);
+INSERT INTO BookInfo VALUES (1781616499, 'The Gift of Imperfection', 'Brene', 'Brown', 'Penguin Random House', 19.67, 2, 7);
+INSERT INTO BookInfo VALUES (1780143111, 'The Laws of Human Nature', 'Patrick', 'King', 'Penguin Books', 29.70, 6, 5);
+INSERT INTO BookInfo VALUES (1781949759, 'The Mountain is You', 'Brianna', 'Wiest', 'Thought Catalog Books', 17.50, 1, 7);
+INSERT INTO BookInfo VALUES (1781305077, 'Forensic Science: Fundamentals & Investigation', 'Bertino', 'Bertino', 'Cengage Learning', 218.00, 5, 0);
+
 # Data entry for employeeInfo
 INSERT INTO employeeInfo
 VALUES (11001, 'Sarah', 'Craig');
