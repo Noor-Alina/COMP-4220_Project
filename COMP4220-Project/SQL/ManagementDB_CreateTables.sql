06a7eed85af1c78a5fb8f5152ede38cf25bfdce6
Alina <An10_23@hotmail.com>
2022-11-12 16:41:42 -0500

    Add tables to database

--- COMP4220-Project/SQL/ManagementDB_CreateTables.sql
+++ COMP4220-Project/SQL/ManagementDB_CreateTables.sql
@@ -11,10 +11,47 @@ CREATE TABLE StudentInfo (
 
 CREATE TABLE BookInfo (
 	book_isbn INTEGER(10) PRIMARY KEY,
-    book_name VARCHAR(200),
+    book_name VARCHAR(50),
     author_fname VARCHAR(30),
     author_lname VARCHAR(30),
+    book_publisher VARCHAR(50),
     book_price DECIMAL(10,2),
-    book_stock INTEGER(10)
+    sell_stock INTEGER(10),
+    loan_stock INTEGER(10)
     );
-    
+
+CREATE TABLE EmployeeInfo (
+	emp_id INTEGER(5) PRIMARY KEY,
+    emp_fname VARCHAR(30),
+    emp_lname VARCHAR(30)
+);
+
+CREATE TABLE loanedBooks(
+	reservation_id INTEGER(5) PRIMARY KEY,
+    book_isbn INTEGER(10),
+    student_id INTEGER(9),
+    loaned_date DATE,
+    due_date DATE,
+    FOREIGN KEY (student_id) REFERENCES StudentInfo(student_id),
+    FOREIGN KEY (book_isbn) REFERENCES BookInfo(book_isbn)
+);
+
+CREATE TABLE PlacedSpecificOrder(
+	order_id INTEGER(5) PRIMARY KEY,
+    student_id INTEGER(9), 
+    book_isbn INTEGER(10),
+    emp_id INTEGER(5),
+    order_date DATE,
+    FOREIGN KEY (student_id) REFERENCES StudentInfo(student_id),
+    FOREIGN KEY (book_isbn) REFERENCES BookInfo(book_isbn),
+    FOREIGN KEY (emp_id) REFERENCES EmployeeInfo(emp_id)
+);
+
+CREATE TABLE OrderInventory(
+	id INTEGER(5) PRIMARY KEY,
+    order_id INTEGER(5),
+    book_isbn INTEGER(10),
+    item_quantity INTEGER(10),
+    arrival_date DATE,
+    FOREIGN KEY (book_isbn) REFERENCES BookInfo(book_isbn)
+);
