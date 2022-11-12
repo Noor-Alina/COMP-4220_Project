4854525fe670644588f2de139d260b4281081de4
Ishmamur Farhan <farha11a@uwindsor.ca>
2022-11-12 17:16:09 -0500

    Added entries for table

--- COMP4220-Project/SQL/ManagementDB_DML.sql
+++ COMP4220-Project/SQL/ManagementDB_DML.sql
@@ -1,7 +1,6 @@
---Insert Into studentInfo
-
 use bookmanagement;
 
+# Data entry for studentInfo
 INSERT INTO studentInfo
 VALUES (167934082, 'John', 'Smith', 'abc12@uwindsor.ca'); 
 INSERT INTO studentInfo
@@ -22,3 +21,25 @@ INSERT INTO studentInfo
 VALUES (182190687, 'Ben', 'Tate', 'TateB4@uwindsor.ca'); 
 INSERT INTO studentInfo
 VALUES (289171390, 'Sarah', 'Jones', 'Jonessa@uwindsor.ca');
+
+# Data entry for employeeInfo
+INSERT INTO employeeInfo
+VALUES (11001, 'Sarah', 'Craig');
+INSERT INTO employeeInfo
+VALUES (91879, 'Arthur', 'Carroll');
+INSERT INTO employeeInfo
+VALUES (62312, 'Catherine', 'Kelly');
+INSERT INTO employeeInfo
+VALUES (62740, 'Adele', 'Howard');
+INSERT INTO employeeInfo
+VALUES (32410, 'Florrie', 'Dixon');
+INSERT INTO employeeInfo
+VALUES (20376, 'Carl', 'Spencer');
+INSERT INTO employeeInfo
+VALUES (55764, 'Albert', 'Crawford');
+INSERT INTO employeeInfo
+VALUES (79009, 'Victoria', 'Morrison');
+INSERT INTO employeeInfo
+VALUES (71358, 'Audrey', 'Payne');
+INSERT INTO employeeInfo
+VALUES (75622, 'Mary', 'Carroll');
