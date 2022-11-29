732a69b9cda57e94781ddc149ad827446d09ef91
Alina <An10_23@hotmail.com>
2022-11-29 00:22:37 -0500

    Change TC

--- COMP4220-Project/src/project/TestClass13.java
+++ COMP4220-Project/src/project/TestClass13.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 package project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,48 +20,3 @@ public class TestClass13 {
 	}
 
 }
-=======
-package project;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.Test;
-
-public class TestClass13 {
-	
-	BookManagement bm = new BookManagement();
-	
-	
-	@Test
-	void testCase1() throws InputException, DatabaseException, SQLException{
-		
-		assertEquals("Book ISBN# 1672895710 Added to Inventory", bm.addInventory(1672895710));
-	}
-	
-	@Test
-	void testCase2(){
-		
-		InputException thrown = assertThrows(
-				InputException.class,
-		           () -> bm.addInventory(2));
-
-		    assertTrue(thrown.getMessage().contains("Invalid Input"));
-	}
-	
-	@Test
-	void testCase3(){
-		
-		DatabaseException thrown = assertThrows(
-				DatabaseException.class,
-		           () -> bm.addInventory(1234567890));
-
-		    assertTrue(thrown.getMessage().contains("Input Not Found"));
-	}
-	
-	
-}
->>>>>>> Stashed changes
